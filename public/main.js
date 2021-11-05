21444aa5ce46472f39234266ba798b909febd325
taeyoungkim <tedisfr@gmail.com>
2021-11-05 21:24:10 +0900

    first commit

---
+++ public/main.js
@@ -0,0 +1,21 @@
+const {app, BrowserWindow, protocol} = require('electron');
+const { prototype } = require('events');
+const path = require('path');
+const url = require('url');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1920,
+        height: 1080
+    });
+
+    const startUrl = process.env.ELECTRON_START_URL || url.format({
+        pathname: path.join(__dirname, '/../build/index.html'),
+        protocol: 'file',
+        slashes: true
+    });
+
+    win.loadURL(startUrl);
+}
+
+app.on('ready', createWindow);
